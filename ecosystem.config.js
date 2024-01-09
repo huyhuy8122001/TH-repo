35efd28c0758123834c011c687b7961fea4ab731
HuyQuangNguyen <huyquangng258@gmail.com>
2024-01-09 23:38:02 +0700

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name: 'my-new-app',
+    script: 'app.js',
+    instance_var: 'INSTANCE_ID',
+    exec_mode: 'fork',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      LOGLEVEL: 'info',
+      HTTP_PORT: 3001,
+      JAMBONZ_ACCOUNT_SID: '9351f46a-678c-43f5-b8a6-d4eb58d131af',
+      JAMBONZ_API_KEY: '1cf2f4f4-64c4-4249-9a3e-5bb4cb597c2a',
+      JAMBONZ_REST_API_BASE_URL: 'http://cpaas61.hq-devlab.cloud',
+      WEBHOOK_SECRET: 'wh_secret_cJqgtMDPzDhhnjmaJH6Mtk',
+      HTTP_PASSWORD: '',
+      HTTP_USERNAME: '',
+    }
+  }]
+};
